4a509b168c74159859bb6344c1e0bf7c813794b9
Walter Perdan <github@kalwaltart.it>
2024-11-05 23:01:14 +0100

    disabling test for now

--- test/NFTMarkerCreator.test.js
+++ test/NFTMarkerCreator.test.js
@@ -2,14 +2,14 @@ const { exec } = require("child_process");
 const path = require("path");
 
 test("NFTMarkerCreator should process the image", (done) => {
-  const scriptPath = path.join(__dirname, "NFTMarkerCreator.js");
-  const imagePath = path.join(__dirname, "pinball.jpg");
+  const scriptPath = path.join(__dirname, "../src/NFTMarkerCreator.js");
+  const imagePath = "../test/pinball-test.jpg";
   const command = `node ${scriptPath} -I ${imagePath}`;
 
   exec(command, (error, stdout, stderr) => {
     expect(error).toBeNull();
     expect(stderr).toBe("");
-    expect(stdout).toContain("Finished marker creation!");
+    expect(stdout).toContain(" Generator finished");
     done();
   });
 }, 200000);
