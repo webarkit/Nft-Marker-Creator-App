e341a62fc86b314fce7dff15b0d5bd35ac7817f6
Walter Perdan <github@kalwaltart.it>
2024-11-06 11:13:44 +0100

    trying to fix test

--- test/NFTMarkerCreator.test.js
+++ test/NFTMarkerCreator.test.js
@@ -3,13 +3,14 @@ const path = require("path");
 
 test("NFTMarkerCreator should process the image", (done) => {
   const scriptPath = path.join(__dirname, "../src/NFTMarkerCreator.js");
+  //const scriptPath =  "./../src/NFTMarkerCreator.js";
   const imagePath = "../test/pinball-test.jpg";
   const command = `node ${scriptPath} -I ${imagePath}`;
 
   exec(command, (error, stdout, stderr) => {
     expect(error).toBeNull();
     expect(stderr).toBe("");
-    expect(stdout).toContain(" Generator finished");
+    expect(stdout).toContain("Create NFT Dataset complete...");
     done();
   });
 }, 50000);
