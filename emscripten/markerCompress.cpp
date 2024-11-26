39b543502cd0eaf674a53656e668babfe492e376
Walter Perdan <github@kalwaltart.it>
2024-11-26 20:07:19 +0100

    fix for issue Malformed zft files #18

--- emscripten/markerCompress.cpp
+++ emscripten/markerCompress.cpp
@@ -6,49 +6,43 @@
 #include <sys/stat.h>
 #endif
 
+#include "zlib/zlib.h"
+#include <AR/ar.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <AR/ar.h>
-#include "zlib/zlib.h"
+
 
 static const char *zipname = "/tempBinFile.bin";
 
-extern "C"
-{
+int compressZip(char *src, int srclen) {
+  FILE *fp;
 
-    uint compressZip(char *src, int srclen)
-    {
-        FILE *fp;
+  char *b = new char[srclen];
 
-        char *b = new char[srclen];
+  printf("Uncompressed size is: %lu", strlen(src));
+  printf("\n----------\n");
 
-        printf("Uncompressed size is: %lu", strlen(src));
-        printf("\n----------\n");
+  z_stream defstream;
+  defstream.zalloc = Z_NULL;
+  defstream.zfree = Z_NULL;
+  defstream.opaque = Z_NULL;
+  defstream.avail_in = (uInt)srclen;
+  defstream.next_in = (Bytef *)src;
+  defstream.avail_out = (uInt)srclen;
+  defstream.next_out = (Bytef *)b;
 
-        z_stream defstream;
-        defstream.zalloc = Z_NULL;
-        defstream.zfree = Z_NULL;
-        defstream.opaque = Z_NULL;
-        defstream.avail_in = (uInt)srclen;
-        defstream.next_in = (Bytef *)src;
-        defstream.avail_out = (uInt)srclen;
-        defstream.next_out = (Bytef *)b;
+  deflateInit(&defstream, Z_BEST_COMPRESSION);
+  deflate(&defstream, Z_FINISH);
+  deflateEnd(&defstream);
 
-        deflateInit(&defstream, Z_BEST_COMPRESSION);
-        deflate(&defstream, Z_FINISH);
-        deflateEnd(&defstream);
+  printf("Compressed size is: %lu\n", strlen(b));
 
-        printf("Compressed size is: %lu\n", strlen(b));
+  fp = fopen(zipname, "wb");
+  fwrite(b, defstream.total_out, 1, fp);
+  fclose(fp);
 
-        fp = fopen(zipname, "wb");
-        fwrite(b, defstream.total_out, 1, fp);
-        fclose(fp);
+  delete[] b;
 
-        delete[] b;
-
-        return 0;
-    }
-
-} // extern "C"
+  return 0;
+}
